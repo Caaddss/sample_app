eeb7e31eeb09edbdc92036d19f30131b316e05d0
dantas <caaddss>
2018-11-20 16:14:54 -0200

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -2,21 +2,27 @@ require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   test "should get home" do
-    get static_pages_home_url
+    get root_path
     assert_response :success
     assert_select 'title', 'Ruby on Rails Tutorial Sample App' 
   end
 
   test "should get helps" do
-    get static_pages_helps_url
+    get help_path
     assert_response :success
     assert_select 'title', 'Ruby on Rails Tutorial Sample App'
   end
 
   test "should get about" do
-    get static_pages_about_url
+    get about_path
     assert_response :success
     assert_select 'title', 'Ruby on Rails Tutorial Sample App'
+  end
+
+  test "should get contact" do
+    get contact_path
+    assert_response :success
+    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
   end
 
 end
